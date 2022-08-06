ae2499a5e94419370fa08d6b7a0a2561cfbf3e03
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-08-06 12:19:22 +0900

    module-service-controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 //*ここにそれぞれのモジュールをimportする
 //*各モジュールはControllerとServiceを持つ
@@ -8,7 +12,7 @@ import { AppService } from './app.service';
 //*Service -> ビジネスロジック Postの処理などを行う
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, TodoModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
